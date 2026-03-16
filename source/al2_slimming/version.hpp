c7cedb9ff5b8ca54b4a632fcf1a7e3744f35826c
hebiiro <io.hebiiro@gmail.com>
2026-03-16 17:27:45 +0900

    🔖r3 2026年03月16日

--- source/al2_slimming/version.hpp
+++ source/al2_slimming/version.hpp
@@ -9,6 +9,6 @@ namespace apn::slimming
 		//
 		// コンストラクタです。
 		//
-		version_t() : version_base_t(L"スリム化MOD", L"🐍スリム化MOD🔖", L"r2") {}
+		version_t() : version_base_t(L"スリム化MOD", L"🐍スリム化MOD🔖", L"r3") {}
 	} version;
 }
